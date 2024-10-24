6a26a473d162b9c3a6900b94da97a11ca9249dd4
Luciano Chi Chan <chanochi.2001@gmail.com>
2024-10-23 18:45:17 -0600

    Archivo conexion y regis-datos realizados

--- php/datos-regist.php
+++ php/datos-regist.php
@@ -1,3 +1,36 @@
 <?php 
+require "conexion.php"; 
 
-?>
+$nombre = $_POST[""];
+$pApellido = $_POST[""];
+$sApellido = $_POST[""];
+$correo = $_POST[""];
+$nCelular = $_POST[""];
+$matricula = $_POST[""];
+$institucion = $_POST[""];
+$carrera = $_POST[""];
+$semestre = $_POST[""];
+$usuario = $_POST[""];
+$contrasena = $_POST[""];
+$contrasenaConfirm = $_POST[""];
+
+$agregar = "INSERT INTO residencia (nombres, p-apellido, s-apellido, correo, n-celular, matricula, institucion, carrera, semestre, usuario, contrasena, contrasena-confirm) VALUES ('$nombre', '$pApellido', '$sApellido', '$correo', '$nCelular', '$matricula'. '$institucion'. '$carrera'. '$semestre'. '$usuario'. '$contrasena'. '$contrasenaConfirm')"; 
+
+$query = mysqli_query ($conectar, $agregar);
+
+if($query){
+    echo '
+    <script>
+    alert ("SE GUARDO CORRECTAMENTE");
+        location.href = "registro.php";
+        </script>
+    ';
+  }else{
+        echo'
+        <script>
+        alert ("NO SE GUARDO CORRECTAMENTE");
+            location.href = "registro.php";
+            </script>
+  
+        ';
+    }
