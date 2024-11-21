5af94fcedebcd0ce3216f3ceb13ccd4ec34bbb71
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2024-11-20 21:01:40 -0600

    avance de sitio administrador

--- php/main/admin/main_admin.php
+++ php/main/admin/main_admin.php
@@ -19,31 +19,19 @@ require "proteccion_admin.php"
   <header class="head-main">
     <h1>
       <?php
-      echo "Bienvenido " . $_SESSION['usuario'];
+      echo "Administrador " . $_SESSION['usuario'];
       ?>
     </h1>
     <a href="#" class="logo"><i class="fa-solid fa-binoculars"></i></a>
     <a href="cerrarsesion_admin.php" class="button-3" onclick="return confirm('¿Estás seguro de que desea salir?')">Salir</a>
   </header>
 
-  <!-- <section class="sect-main">
-    <nav class="nav-main">
-      <ul>
-        <li><a href="main-ubicaciones.html" class="button-1"><i class="fa-solid fa-location-dot"></i></a></li>
-        <li><a href="#" class="button-1"><i class="fa-solid fa-brain"></i></a></li>
-        <li><a href="#" class="button-1"><i class="fa-solid fa-book-bookmark"></i></a></li>
-        <li><a href="#" class="button-1"><i class="fa-solid fa-envelope"></i></a></li>
-        <li><a href="#" class="button-1"><i class="fa-solid fa-database"></i></a></li>
-      </ul>
-    </nav>
-  </section> -->
-
   <?php
   include "menu_admin.php";
   ?>
 
-  <article class="arti-main">
-    <h2>3DPrinter Explorer</h2>
+  <article class="arti-main-intro">
+    <h1>3DPrinter Explorer</h1>
   </article>
 
 </body>
