e271a0838a8e2bf04b277f9ff628287c4168f897
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2024-11-24 17:46:14 -0600

    estilos al modulo de guias

--- php/main/main-guias.php
+++ php/main/main-guias.php
@@ -9,8 +9,8 @@ require "proteccion.php"
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>3DPrinter Explorer 2024</title>
-  <link rel="stylesheet" href="../../css/main/styles-main.css">
-  <link rel="stylesheet" href="../../css/main/styles-main-guias.css">
+  <link rel="stylesheet" href="../../css/main/styles-main.css?v=1">
+  <link rel="stylesheet" href="../../css/main/styles-main-guias.css?v=1">
   <script src="https://kit.fontawesome.com/892bb677d3.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 </head>
@@ -26,148 +26,151 @@ require "proteccion.php"
   include "menu.php";
   ?>
 
-
-  <article class="arti-main">
-    <figure>
-      <p>REPOSITORIO GRABCAD</p>
-      <a href="https://grabcad.com/library" target="_blank"><img src="../../pictures/grabcad-removebg-preview.png" alt=""></a>
-    </figure>
-
-    <figure>
-      <p>REPOSITORIO VIDEOS GRABCAD</p>
-      <a href="https://grabcad.com/tutorials" target="_blank"><img src="../../pictures/grabcad-removebg-preview.png" alt=""></a>
-    </figure>
-  </article>
-
-
-
-  <section class="sec-pdf ancho">
-    <article class="art-pdf" id="btn_modal">
+  <section class="sec-pdf">
+    <article class="art-pdf" id="btn_modal1">
       <figure>
         <img src="..\..\pictures\guiaM1.png" alt="">
       </figure>
       <div class="pdf-info">
-        <p><span>TITULO:</span> PROTOCOLO DE PROYECTO DE RESIDENCIA </p><br>
-        <p><span>DESCRIPCION:</span> El Protocolo de Proyecto de Residencia es un documento formal que describe detalladamente el proyecto que un estudiante llevará a cabo durante su periodo de residencia profesional. </p>
-        <a href="#" id="btn_modal1">Ver documento</a>
+        <p><span>TITULO:</span> </p><br>
+        <p><span>DESCRIPCIÓN:</span> </p>
       </div>
     </article>
 
-    <article class="art-pdf">
+    <article class="art-pdf" id="btn_modal2">
       <figure>
         <img src="..\..\pictures\guiaM2.png" alt="">
       </figure>
       <div class="pdf-info">
-        <p><span>TITULO:</span> PROTOCOLO DE PROYECTO DE RESIDENCIA </p><br>
-        <p><span>DESCRIPCION:</span> El Protocolo de Proyecto de Residencia es un documento formal que describe detalladamente el proyecto que un estudiante llevará a cabo durante su periodo de residencia profesional. </p>
-        <a href="#" id="btn_modal2">Ver documento</a>
+        <p><span>TITULO:</span> </p><br>
+        <p><span>DESCRIPCIÓN:</span> </p>
       </div>
     </article>
-    <article class="art-pdf">
+
+    <article class="art-pdf" id="btn_modal3">
       <figure>
         <img src="..\..\pictures\guia_STL.png" alt="">
       </figure>
       <div class="pdf-info">
-        <p><span>TITULO:</span> PROTOCOLO DE PROYECTO DE RESIDENCIA </p><br>
-        <p><span>DESCRIPCION:</span> El Protocolo de Proyecto de Residencia es un documento formal que describe detalladamente el proyecto que un estudiante llevará a cabo durante su periodo de residencia profesional. </p>
-        <a href="#" id="btn_modal3">Ver documento</a>
+        <p><span>TITULO:</span> </p><br>
+        <p><span>DESCRIPCIÓN:</span> </p>
       </div>
-    </article>  
-  </section>  
+    </article>
+  </section>
 
-   <!-- DOCUMENTO 1 -->
+
+  <!-- ------------------------------------- -->
+
+  <!-- DOCUMENTO 1 -->
   <section class="modal_padre" id="cont_modal1">
-      <article class="modal">
-      <div class="cerrar" id="cerrar_modal1">
-          <a href="#"><i class="fa-solid fa-x"></i></a>
-        </div>
-        <iframe src=" ../../docs/modulo1.pdf" title="PDF Viewer"></iframe>
-      </article>
-    </section>
+    <article class="modal">
+      <div class="cerrar">
+        <h2>TITULO</h2>
+        <a href="#" id="cerrar_modal1"><i class="fa-solid fa-circle-xmark"></i></a>
+      </div>
+      <iframe src=" ../../docs/modulo1.pdf" title="PDF Viewer"></iframe>
+    </article>
+  </section>
 
-    <!-- DOCUMENTO 2 -->
+  <!-- DOCUMENTO 2 -->
 
-    <section class="modal_padre" id="cont_modal2">
-      <article class="modal">
-      <div class="cerrar" id="cerrar_modal2">
-          <a href="#"><i class="fa-solid fa-x"></i></a>
-        </div>
-        <iframe src=" ../../docs/modulo2.pdf" title="PDF Viewer"></iframe>
-      </article>
-    </section>
+  <section class="modal_padre" id="cont_modal2">
+    <article class="modal">
+      <div class="cerrar">
+        <h2>TITULO</h2>
+        <a href="#" id="cerrar_modal2"><i class="fa-solid fa-circle-xmark"></i></a>
+      </div>
+      <iframe src=" ../../docs/modulo2.pdf" title="PDF Viewer"></iframe>
+    </article>
+  </section>
 
-    <!-- DOCUMENTO 3 -->
+  <!-- DOCUMENTO 3 -->
 
-    <section class="modal_padre" id="cont_modal3">
-      <article class="modal">
-      <div class="cerrar" id="cerrar_modal3">
-          <a href="#"><i class="fa-solid fa-x"></i></a>
-        </div>
-        <iframe src=" ../../docs/GUIA_STL.pdf" title="PDF Viewer"></iframe>
-      </article>
-    </section>
+  <section class="modal_padre" id="cont_modal3">
+    <article class="modal">
+      <div class="cerrar">
+        <h2>TITULO</h2>
+        <a href="#" id="cerrar_modal3"><i class="fa-solid fa-circle-xmark"></i></a>
+      </div>
+      <iframe src=" ../../docs/GUIA_STL.pdf" title="PDF Viewer"></iframe>
+    </article>
+  </section>
 
+  <!-- ------------------------------------- -->
+
+  <article class="arti-main">
+    <figure>
+      <p>REPOSITORIO MODELOS GRABCAD</p>
+      <a href="https://grabcad.com/library" target="_blank"><img src="../../pictures/grabcad-removebg-preview.png" alt=""></a>
+    </figure>
+
+    <figure>
+      <p>REPOSITORIO TUTORIALES GRABCAD</p>
+      <a href="https://grabcad.com/tutorials" target="_blank"><img src="../../pictures/grabcad-removebg-preview.png" alt=""></a>
+    </figure>
+  </article>
 
   <script>
     // SCRIPT DOCUMENTO 1
-    $(document).ready(function () {
-  $("#btn_modal1").click(function (e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    $("#cont_modal1").addClass('mostrar');
-  });
+    $(document).ready(function() {
+      $("#btn_modal1").click(function(e) {
+        e.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        $("#cont_modal1").addClass('mostrar');
+      });
 
-  $("#cerrar_modal1").click(function (e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado
-    $("#cont_modal1").removeClass('mostrar');
-  });
+      $("#cerrar_modal1").click(function(e) {
+        e.preventDefault(); // Evita el comportamiento predeterminado
+        $("#cont_modal1").removeClass('mostrar');
+      });
 
-  // Cerrar el modal al hacer clic fuera del contenido
-  $(window).click(function (e) {
-    if ($(e.target).is("#cont_modal1")) {
-      $("#cont_modal1").removeClass('mostrar');
-    }
-  });
-});
+      // Cerrar el modal al hacer clic fuera del contenido
+      $(window).click(function(e) {
+        if ($(e.target).is("#cont_modal1")) {
+          $("#cont_modal1").removeClass('mostrar');
+        }
+      });
+    });
 
-  //SCRIPT DOCUMENTO 2
-  $(document).ready(function () {
-  $("#btn_modal2").click(function (e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    $("#cont_modal2").addClass('mostrar');
-  });
+    //SCRIPT DOCUMENTO 2
+    $(document).ready(function() {
+      $("#btn_modal2").click(function(e) {
+        e.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        $("#cont_modal2").addClass('mostrar');
+      });
 
-  $("#cerrar_modal2").click(function (e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado
-    $("#cont_modal2").removeClass('mostrar');
-  });
+      $("#cerrar_modal2").click(function(e) {
+        e.preventDefault(); // Evita el comportamiento predeterminado
+        $("#cont_modal2").removeClass('mostrar');
+      });
 
-  // Cerrar el modal al hacer clic fuera del contenido
-  $(window).click(function (e) {
-    if ($(e.target).is("#cont_modal2")) {
-      $("#cont_modal2").removeClass('mostrar');
-    }
-  });
-});
+      // Cerrar el modal al hacer clic fuera del contenido
+      $(window).click(function(e) {
+        if ($(e.target).is("#cont_modal2")) {
+          $("#cont_modal2").removeClass('mostrar');
+        }
+      });
+    });
 
-// SCRIPT DOCUMENTO 3
-$(document).ready(function () {
-  $("#btn_modal3").click(function (e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    $("#cont_modal3").addClass('mostrar');
-  });
+    // SCRIPT DOCUMENTO 3
+    $(document).ready(function() {
+      $("#btn_modal3").click(function(e) {
+        e.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        $("#cont_modal3").addClass('mostrar');
+      });
 
-  $("#cerrar_modal3").click(function (e) {
-    e.preventDefault(); // Evita el comportamiento predeterminado
-    $("#cont_modal3").removeClass('mostrar');
-  });
+      $("#cerrar_modal3").click(function(e) {
+        e.preventDefault(); // Evita el comportamiento predeterminado
+        $("#cont_modal3").removeClass('mostrar');
+      });
 
-  // Cerrar el modal al hacer clic fuera del contenido
-  $(window).click(function (e) {
-    if ($(e.target).is("#cont_modal3")) {
-      $("#cont_modal3").removeClass('mostrar');
-    }
-  });
-});
+      // Cerrar el modal al hacer clic fuera del contenido
+      $(window).click(function(e) {
+        if ($(e.target).is("#cont_modal3")) {
+          $("#cont_modal3").removeClass('mostrar');
+        }
+      });
+    });
   </script>
 </body>
+
 </html>
